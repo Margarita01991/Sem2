c77dfdc10503620f09b03f55d89e5d04c4f383d5
Margarita <margarita27011991@gmail.com>
2022-08-09 17:08:48 +0300

    add line "consol clear" to task11

--- task11/Program.cs
+++ task11/Program.cs
@@ -1,10 +1,12 @@
 ﻿// Программа которая выводит случайное трехзначное число и удаляет вторую цифру из этого числа. 456 -> 46; 782 -> 72; 918 -> 98
+Console.Clear();
 int number = new Random().Next(99, 1000);
 Console.WriteLine($"Наше случайное число: {number}");
 int digit1 = number/100; //нахождение первого числа
 int digit2 = number%10;
-Console.WriteLine($"Первая цифра: {digit1}");
-Console.WriteLine($"Последняя цифра: {digit2}");
-int result = digit1 * 10;
-int sum = result + digit2;
-Console.WriteLine($"Наше число: {sum}");
+Console.WriteLine($"{digit1}{digit2}");
+//Console.WriteLine($"Первая цифра: {digit1}");
+//Console.WriteLine($"Последняя цифра: {digit2}");
+//int result = digit1 * 10;
+//int sum = result + digit2;
+//Console.WriteLine($"Наше число: {sum}");
