ceb63fa9ec9d2e9c95012162d36cc388b20f02da
Margarita <margarita27011991@gmail.com>
2022-08-09 11:56:57 +0300

    add task9

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,2 +1,15 @@
-﻿// Программа которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
-Console.WriteLine("Hello, World!");
+﻿// Программа которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа 78 -> 8; 12 -> 2; 85 -> 8
+int number = new Random().Next(10, 100);// последнее число не входит в отрезок!!!
+Console.WriteLine($"Наше случайное число: {number}");//Console.WriteLine(number);
+int digit1 = number/10; //нахождение первого числа
+int digit2 = number%10; //нахождение последнего числа
+Console.WriteLine($"Первая цифра: {digit1}");
+Console.WriteLine($"Последняя цифра: {digit2}");
+if (digit1 >digit2)
+{
+    Console.WriteLine($"Наибольшая цифра: {digit1}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра: {digit2}");
+}
